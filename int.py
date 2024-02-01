19d367be88517989ad906c99f1666111df3e3013
Realencod <157638504+Realencod@users.noreply.github.com>
2024-02-01 15:38:10 +0000

    changed the code from float to int

--- int.py
+++ int.py
@@ -1,5 +1,12 @@
-pi = float(input("Enter the value of pi to 5 d.p:"))
-if pi == 3.14159:
-  print("That is correct! Well done!")
-else:
-  print("I'm not being rude or anything but you could use a little bit of help from a calculator.")
+print("=======GENERATION GENERATOR========")
+gender = input("What should I refer you as....." ,"\033[33m","male" ,"\033[0m" ,"," ,"\033[33m" ,"female" ,"\033[0m" ,"or" ,"\033[33m" ,"they?")
+year = int(input("What year were you born?"))
+if year >=1926 and year<=1946:
+  print("Traditionalist...be kind enough as to share your wisdom with us young ones")
+elif year >= 1947 and year < 1964 and gender == "female" or gender == "Female":
+  print("Hello grandma!")
+elif year >= 1947 and year < 1964 and gender == "male" or gender == "Male":
+  print("Hello grandpa!")
+elif year >= 1947 and year < 1964 and gender == "they" or gender == "They":
+  print("Woah ,I'm surprised that this option existed in your time")
+
